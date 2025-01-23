4352e82b3e750a94587c0512d2e9f65a9a85d3d5
Charan Sai Reddy W <72861534+charanwudaru@users.noreply.github.com>
2025-01-23 11:01:18 +0530

    Add files via upload

---
+++ encapsulationage.java
@@ -0,0 +1,38 @@
+public class encapsulationage {
+    private int age;
+
+    public encapsulationage(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age.");
+        }
+    }
+
+    public void check() {
+        if (age >= 18) {
+            System.out.println("Eligible to vote.");
+        } else {
+            System.out.println("Not eligible to vote.");
+        }
+    }
+
+    public static void main(String[] args) {
+        encapsulationage ob1 = new encapsulationage(18);
+        ob1.check();
+        System.out.println(ob1.getAge());
+
+
+        encapsulationage ob2 = new encapsulationage(19);
+        ob2.check();
+        System.out.println(ob2.getAge());
+    }
+}
